4ba2c4be1de41235d90af558b1c39cdb6dd4dda5
Parth Gohel <parth.gohel@mindinventory.com>
2022-05-10 10:44:08 +0530

    -v1.0.0

---
+++ TopTabBarView/Source/UIView+Extension.swift
@@ -0,0 +1,18 @@
+//
+//  UIView+Extension.swift
+//  CustomTopBarDemo
+//
+//  Created by Parth Gohel on 06/05/22.
+//
+
+import UIKit
+
+extension UIView {
+
+    func viewCenter(usePresentationLayerIfPossible: Bool) -> CGPoint {
+        if usePresentationLayerIfPossible, let presentationLayer = layer.presentation() {
+            return presentationLayer.position
+        }
+        return center
+    }
+}
